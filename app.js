8e56812abaf3f344b46ca2e25696c640d15f334c
605258778 <605258778@qq.com>
2016-04-11 10:53:13 +0800

    添加：登录功能模块

--- app.js
+++ app.js
@@ -32,12 +32,41 @@ app.get('/',function(req,res){
 	})
 })
 
+//signin
+app.post('/user/signin',function(req,res) {
+  var _user = req.body.user
+  var name = _user.name
+  var password = _user.password
 
+  User.findOne({name: name}, function(err, user) {
+    if (err) {
+      console.log(err)
+    }
+
+    if (!user) {
+      return res.redirect('/signup')
+    }
+
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {
+        console.log(err)
+      }
+
+      if (isMatch) {
+        req.session.user = user
+
+        return res.redirect('/')
+      }
+      else {
+        return res.redirect('/signin')
+      }
+    })
+  })
+})
 
 //signup
 app.post('/user/signup',function(req,res) {
     var _user = req.body.user
-
   User.findOne({name: _user.name},  function(err, user) {
     if (err) {
       console.log(err)
